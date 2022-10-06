b33ad0ae025d4af6f68884b7e8bb6ab29574a0ad
pikiha52 <pikiha52@gmail.com>
2022-10-06 22:28:26 +0700

    add:check token in app

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,33 @@
   <!-- <nav>
     <router-link to="/">Login</router-link>
   </nav> -->
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+import axios from 'axios'
+import router from './router';
+
+export default {
+  name: "App",
+  methods: {
+    checkLogin: function () {
+      if (localStorage.getItem("token") != null) {
+        axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("token");
+        axios.get(`http://localhost:3000/api/v1/auth/checktoken`)
+          .then((response) => {
+            response
+          }).catch((error) => {
+            console.log(error)
+            router.push('/')
+          })
+      } else {
+        router.push('/')
+      }
+    }
+  },
+  mounted() {
+    this.checkLogin()
+  }
+}
+</script>
